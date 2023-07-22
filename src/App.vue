b1abdef70d7e958ffa91f58b8e9a230588187374
Long Le <bestoflong123@gmail.com>
2023-07-22 15:18:05 +0700

    new HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup>
-<<<<<<< HEAD
 import { RouterLink, RouterView } from "vue-router";
 
 import HelloWorld from "./components/HelloWorld.vue";
-import HelloWorld1 from "./components/HelloWorld.vue";
+import HelloWorld1 from "./components/HelloWorld1.vue";
 </script>
 
 <template>
@@ -12,34 +11,3 @@ import HelloWorld1 from "./components/HelloWorld.vue";
 </template>
 
 <style scoped></style>
-=======
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
->>>>>>> 2e31b4ded6198b2c3cf18989fa1d4120327a63c7
