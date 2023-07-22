b651a203757820ec9adf0551f9ba4cfde20a9ff2
Long Le <bestoflong123@gmail.com>
2023-07-22 14:53:44 +0700

    Long push

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+
+import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld1 from "./components/HelloWorld.vue";
+</script>
+
+<template>
+	<HelloWorld></HelloWorld>
+	<HelloWorld1></HelloWorld1>
+</template>
+
+<style scoped></style>
