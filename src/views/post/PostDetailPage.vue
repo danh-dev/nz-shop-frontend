507ff7a986786d7d8b226ecc37991802fb4ff058
Sangdang2203 <sangdang2203@gmail.com>
2023-08-15 22:58:02 +0700

    1st commit

--- src/views/post/PostDetailPage.vue
+++ src/views/post/PostDetailPage.vue
@@ -1,7 +1,52 @@
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 const articles = ref([
-  
+  {
+    id: 1,
+    image: "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/07/Danh-gia-Xiaomi-Redmi-Note-12-Pro-5G-6-1.jpg",
+    photo: "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/07/Danh-gia-Xiaomi-Redmi-Note-12-Pro-5G-6-1.jpg",
+    title: "Đã có điểm thi đại học, dưới 10 triệu nên xin cha mẹ mua smartphone vừa hợp túi tiền vừa xịn?",
+    author: "John",
+    time: "04/08/2023",
+
+  },
+  {
+    id: 2,
+    image: "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/06/Galaxy-A23-5G-7.jpeg",
+    photo: "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/06/Galaxy-A23-5G-7.jpeg",
+    title: "Top smartphone tầm trung có màn hình lớn xem phim tốt nhất hiện nay.",
+    author: "John",
+    time: " 03/08/2023",
+
+  },
+  {
+    id: 3,
+    image: "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/05/thoi-nghe-18-5.jpg",
+    photo: "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/05/thoi-nghe-18-5.jpg",
+    title: "#Thời nghệ 18/5: Tìm hiểu giá iPhone trên Apple Store Online tại VN, HTC trở lại đường đua?",
+    author: "John",
+    time: " 01/08/2023",
+
+  },
+  {
+    id: 4,
+    image: "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/04/Xiaomi-Redmi-Note-12-Pro-1-1.jpg",
+    photo: "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/04/Xiaomi-Redmi-Note-12-Pro-1-1.jpg",
+    title: "Đánh giá chi tiết Xiaomi Redmi Note 12 Pro 5G: Xiaomi khác hay người dùng xưa và nay đã khác?",
+    author: "John",
+    time: "31/07/2023",
+
+  },
+  {
+    id: 5,
+    image: "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/05/Xiaomi-13-Lite-cover.jpeg",
+    photo: "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/05/Xiaomi-13-Lite-cover.jpeg",
+    title: "4 chiếc smartphone 5G cho trải nghiệm tuyệt vời giá dưới 10 triệu đáng mua nhất tháng 8.",
+    author: "John",
+    time: "31/07/2023",
+
+  },
+
 ]);
 
 </script>
@@ -43,14 +88,6 @@ const articles = ref([
             </div>
           </v-sheet>
 
-          <v-btn color="danger" variant="flat" class="mt-3 text-h6 rounded-xl">Bài viết liên quan</v-btn>
-          <v-sheet>
-            <div class="d-flex align-center w-50 my-3" v-for="relatedArticle in relatedArticles" :key="relatedArticle.id">
-              <img :src="relatedArticle.image" class="w-25 rounded" />
-              <a :href="`/news/${getSlugByName(relatedArticle.title)}`" class="text-caption px-1">{{
-                relatedArticle.title }}</a>
-            </div>
-          </v-sheet>
         </v-sheet>
       </v-col>
     </v-row>
