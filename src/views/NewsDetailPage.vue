660309c72913e7297f3b510cb8f769d3df4464d9
Sangdang2203 <sangdang2203@gmail.com>
2023-08-23 20:39:22 +0700

    4th commit

--- src/views/NewsDetailPage.vue
+++ src/views/NewsDetailPage.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, onBeforeMount } from "vue";
+import { ref, onBeforeMount, onMounted } from "vue";
+import axios from "axios";
 import router from "../router/index.js";
 import useNewsStore from "../stores/useNewsStore.js";
 import { storeToRefs } from "pinia";
@@ -7,14 +8,28 @@ import getSlugByName from "../utils/getSlugByName.js";
 import NewsSideBar from "../components/news/NewsSideBar.vue";
 
 const { relatedArticles } = storeToRefs(useNewsStore());
+const url = "http://127.0.0.1:8000/";
+const posts = ref([]);
+const fetchPost = async () => {
+  try {
+    const response = await axios.get(`${url}api/posts`);
+    if (response.data.status === 200) {
+      posts.value = response.data.data.reverse();
+    }
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
 
-const article = ref(null);
+onMounted(fetchPost);
+
+// const article = ref(null);
 
 const { findArticleBySlug } = useNewsStore();
 
 onBeforeMount(() => {
   const newsName = router.currentRoute.value.params.newsName;
-  article.value = findArticleBySlug(newsName);
+  posts.value = findArticleBySlug(newsName);
 });
 
 
@@ -27,24 +42,24 @@ onBeforeMount(() => {
         <NewsSideBar />
       </v-col>
 
-      <v-col :cols="12" lg="9" md="9">
+      <v-col :cols="12" lg="9" md="9" v-for="item in posts" :key="item.id">
         <div>
-          <v-img :src="article.image" class="rounded-lg mb-2" />
+          <v-img :src="item.image" class="rounded-lg mb-2" />
         </div>
         <v-sheet class="border rounded-xl px-4 text-justify mx-10" style="position: relative; top: -80px;">
           <v-btn color="danger" variant="flat" class="my-2 text-h6 text-white rounded-xl">Tin tức</v-btn>
-          <h2>{{ article.title }}</h2>
+          <h2>{{ item.title }}</h2>
 
           <v-sheet class="d-flex align-center my-3">
             <img src="/assets/unknow.png" class="rounded-circle" style="width: 40px; height: 40px;">
             <div class="mx-2">
-              <h5>{{ article.author }}</h5>
-              <p class="text-caption">{{ article.time }}</p>
+              <h5>{{ item.author }}</h5>
+              <p class="text-caption">{{ item.created_at }}</p>
             </div>
           </v-sheet>
 
           <v-sheet>
-            <p class="py-2">{{ article.description }}</p>
+            <p class="py-2">{{ item.content }}</p>
           </v-sheet>
 
           <v-sheet>
--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
+import axios from "axios";
 import useNewsStore from "../stores/useNewsStore.js";
 import { storeToRefs } from "pinia";
 import getSlugByName from "../utils/getSlugByName.js";
@@ -10,11 +11,27 @@ import { useDisplay } from "vuetify/lib/framework.mjs";
 const { xs } = useDisplay();
 const { articles, slides, subArticles } = storeToRefs(useNewsStore());
 
+const url = "http://127.0.0.1:8000/";
+const posts = ref([]);
+const fetchPost = async () => {
+  try {
+    const response = await axios.get(`${url}api/posts`);
+    if (response.data.status === 200) {
+      posts.value = response.data.data.reverse();
+    }
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
+onMounted(fetchPost);
+
+
 // const articles = ref([]);
 const page = ref(1);
 const rowsPerPage = 10;
 const numberOfPage = computed(() => {
-  return Math.ceil(articles.value.length / rowsPerPage);
+  return Math.ceil(posts.value.length / rowsPerPage);
 });
 const updatePage = (event) => {
   page.value = event;
@@ -55,16 +72,16 @@ const updatePage = (event) => {
     </v-row>
 
     <v-row>
-      <v-col :cols="12" lg="8" md="8" v-if="articles.length > 0">
+      <v-col :cols="12" lg="8" md="8" v-if="posts.length > 0">
         <h4 class="py-2 text-danger">Tin tức cập nhật</h4>
-        <v-sheet v-for="article in articles.slice((page - 1) * rowsPerPage, page * rowsPerPage)" :key="article.id"
+        <v-sheet v-for="item in posts.slice((page - 1) * rowsPerPage, page * rowsPerPage)" :key="item.id"
           class="d-flex mb-3 text-body-1 border rounded-lg w-100" rounded="3" style="width: 60%;">
-          <img class="w-50 me-2 rounded-lg" :src="article.image" />
+          <v-img class="w-50 me-2 rounded-lg" :src="item.image" />
           <div class="d-flex flex-column justify-space-between py-1">
-            <a :href="`/news/${getSlugByName(article.title)}`" class="">{{ article.title }}</a>
+            <a :href="`/news/${getSlugByName(item.title)}`" class="">{{ item.title }}</a>
             <div class="d-flex align-center text-muted">
               <v-icon size="16">mdi-clock-outline</v-icon>
-              <p class="px-1 text-caption">{{ article.time }}</p>
+              <p class="px-1 text-caption">{{ item.created_at.slice(0, 10) }}</p>
             </div>
           </div>
         </v-sheet>
@@ -73,16 +90,16 @@ const updatePage = (event) => {
       <v-col cols="12" lg="4" md="4">
         <v-sheet class="col-12">
           <h4 class="px-2 py-1 text-danger">Xem nhiều tuần qua</h4>
-          <div v-for="subArticle in subArticles" :key="subArticle.id" class="my-2 w-75 px-2">
-            <img :src="subArticle.image" alt="" class="w-100 h-50 rounded-lg">
+          <div v-for="item in posts.slice(8, 12)" :key="item.id" class="my-2 w-75 px-2">
+            <img :src="item.image" alt="" class="w-100 h-50 rounded-lg"/>
             <div>
-              <a :href="`/news/${getSlugByName(subArticle.title)}`" class="text-caption">{{ subArticle.title }}</a>
+              <a :href="`/news/${getSlugByName(item.title)}`" class="text-caption">{{ item.title }}</a>
             </div>
           </div>
         </v-sheet>
       </v-col>
     </v-row>
-    <GlobalPagination v-if="articles.length > rowsPerPage" :numberOfPages="numberOfPage" :page="page"
+    <GlobalPagination v-if="posts.length > rowsPerPage" :numberOfPages="numberOfPage" :page="page"
       @update:page="updatePage" />
   </v-sheet>
 </template>
