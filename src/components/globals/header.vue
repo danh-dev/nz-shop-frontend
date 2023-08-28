a39ac6cfe3e79c422f5102dcdc99db898e378dcf
danh-fptaptech <danhnhts2210026@fpt.edu.vn>
2023-08-29 05:33:41 +0700

    Before Merge Long

--- src/components/globals/header.vue
+++ src/components/globals/header.vue
@@ -1,23 +1,39 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
 import { useCartStore } from "@/stores/cart";
+import { userData } from "../../stores/userData";
+import axios from "axios";
+import { ref } from "vue";
 
 const cartStore = useCartStore();
+const userStore = userData();
+
+const errors = ref([])
+const logout = async () => {
+  try {
+    await axios.post('logout', {
+      user_id: userStore.id,
+    });
+    userStore.logout();
+    localStorage.removeItem("accessToken");
+  } catch (e) {
+    errors.value = e.response.data.errors
+  }
+};
 </script>
 
 <template>
   <v-app-bar class="bg-red-accent-4" elevation="0">
     <v-container>
       <v-row>
-        <v-col cols="4" class="d-flex align-center justify-space-between">
+        <v-col cols="4" class="d-flex align-center ">
           <a @click="$router.push('/')" class="m-pointer">
-            <img id="logoShop" src="/NZShop.svg" alt="logo shop">
+            <img id="logoShop" src="/assets/NZShop.svg" alt="logo shop">
           </a>
           <v-btn class="menu-list text-body-2" prepend-icon="mdi-format-list-bulleted">
             Danh mục
           </v-btn>
         </v-col>
-        <v-col cols="4" class="d-flex align-center justify-space-between">
+        <v-col cols="4" class="d-flex align-center justify-content-between">
           <v-form id="searchBox" @submit.prevent>
             <v-text-field density="compact" variant="solo" label="Bạn cần tìm gì?" prepend-inner-icon="mdi-magnify"
               single-line hide-details></v-text-field>
@@ -34,8 +50,9 @@ const cartStore = useCartStore();
             </v-badge>
             Giỏ hàng
           </v-btn>
-          <v-btn density="compact" class="btn-login text-body-2" prepend-icon="mdi-account-circle-outline" stacked>
-            Đăng nhập
+          <v-btn @click="userStore.full_name ? logout() : $router.push('/login')" density="compact"
+            class="btn-login text-body-2" prepend-icon="mdi-account-circle-outline" stacked>
+            {{ userStore.full_name ? userStore.full_name : "Đăng nhập" }}
           </v-btn>
         </v-col>
       </v-row>
@@ -55,7 +72,7 @@ const cartStore = useCartStore();
 .btn-login,
 .menu-list {
   text-transform: none;
-  background-color:  #c50000;
+  background-color: #df3346;
   margin: 0 10px;
 }
 </style>
