67bf556c9ce76cebabefc4f0ca7c3d0de2c744f4
Sangdang2203 <sangdang2203@gmail.com>
2023-09-09 16:57:13 +0700

    Sang commit v5

--- src/views/visitorLayout.vue
+++ src/views/visitorLayout.vue
@@ -1,64 +1,55 @@
 <script setup>
-import {useDisplay} from "vuetify";
-import {RouterView, useRoute} from "vue-router";
+import { useDisplay } from "vuetify";
+import { RouterView } from "vue-router";
 
-const route = useRoute();
-
+import TheHeader from "@/components/layouts/TheHeader.vue";
 import TheFooter from "@/components/layouts/TheFooter.vue";
 import FullscreenModal from "@/components/modals/FullscreenModal.vue";
 import MenuModal from "@/components/modals/MenuModal.vue";
-import Header from "@/components/globals/Header.vue";
 
-const {lgAndUp} = useDisplay();
-
+const { lgAndUp } = useDisplay();
 </script>
 
 <template>
   <v-layout>
-    <Header/>
+    <TheHeader />
 
     <v-main
-        style="position: relative"
-        id="modal-container"
+      style="position: relative"
+      id="modal-container"
     >
       <v-container :fluid="!lgAndUp">
-        <main>
-          <router-view/>
-        </main>
+        <RouterView />
       </v-container>
     </v-main>
 
-    <TheFooter/>
+    <TheFooter />
 
     <FullscreenModal
-        location-strategy="static"
-        scroll-strategy="close"
-        :scrim="false"
-        transition="dialog-bottom-transition"
-        activator="#category"
-        absolute
-        content-class="position-fixed"
-        attach="#modal-container"
-        width="100%"
-        height="calc(100vh - 56px)"
+      location-strategy="static"
+      scroll-strategy="close"
+      :scrim="false"
+      transition="dialog-bottom-transition"
+      activator="#category"
+      absolute
+      content-class="position-fixed"
+      attach="#modal-container"
+      width="100%"
+      height="calc(100vh - 56px)"
     />
 
     <MenuModal
-        style="top: 64px"
-        location-strategy="static"
-        scroll-strategy="none"
-        activator="#categories"
-        attach="#modal-container"
-        transition="fade-transition"
-        width="100%"
-        absolute
-        content-class="position-fixed"
+      style="top: 64px"
+      location-strategy="static"
+      scroll-strategy="none"
+      activator="#categories"
+      attach="#modal-container"
+      transition="fade-transition"
+      width="100%"
+      absolute
+      content-class="position-fixed"
     />
   </v-layout>
 </template>
 
-<style scoped>
-main {
-  min-height: 83vh;
-}
-</style>
+<style></style>
